1dc3268867cbe9d6fe9a7bdfa2981c360ae71080
Yeralkhan <Yeralkhan@mail.fr>
2026-02-20 10:18:46 +0500

    Был изменен Надпись в функций print

--- bin/week5.dart
+++ bin/week5.dart
@@ -2,6 +2,6 @@ import 'package:week5/week5.dart' as week5;
 
 void main(List<String> arguments) {
   print('Hello world: ${week5.calculate()}!');
-  print('Blitzkrig');
-  print('Blitzkrig2');
+  print('Rest in');
+  print('Peace');
 }
